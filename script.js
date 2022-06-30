eedc9dddd8a483ff56aedd0bd735eb3cef9896f1
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-06-30 12:07:02 +0530

    Added playRound function that plays one round of the game and returns a result as a string.

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
-function computerPlay
+// Function to assign computer a selection
+function computerPlay()
 {
     let random = Math.floor(Math.random() * 3);
     if (random == 0)
     {
-        return "Rock";
+        return "rock";
     }
     else if (random == 1)
     {
-        return "Paper";
+        return "paper";
     }
     else
     {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 let computerSelecton = computerPlay();
 console.log(computerSelecton);
+let playerSelection = prompt("Rock - Paper - Scissors : Enter Pick : ").toLowerCase();
+
+// Function to play a round of Rock Paper Scissors
+function playRound(playerSelection,computerSelecton)
+{
+    if (playerSelection == computerSelecton)
+    {
+        return "Tie";
+    }
+    else if(playerSelection == "rock")
+    {
+        if(computerSelecton == "paper")
+            return "Lose";
+        else
+            return "Win";
+    }
+    else if(playerSelection == "paper")
+    {
+        if(computerSelecton == "rock")
+            return "Win";
+        else
+            return "Lose";
+    }
+    else if(playerSelection == "scissors")
+    {
+        if(computerSelecton == "rock")
+            return "Lose";
+        else
+            return "Win";
+    }
+
+}
